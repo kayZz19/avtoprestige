0eb0088e37a49f76e2bd712507c9a9ac6e95f9ed
Roman Kirillov <roman.kirillov.06@mail.ru>
2026-06-10 04:51:54 +0300

    Renamd main script

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ import "./styles/animations.css";
 import "./styles/headerMenu.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/avtoshkola-avtoprestige">
+  <BrowserRouter basename="/avtoshkola">
     <App />
   </BrowserRouter>,
 );
